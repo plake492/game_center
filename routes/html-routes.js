9134d6f7f42ab94406c7e1a196230d6839f3b592
plake <plake492@gmail.com>
2020-05-03 09:27:25 -0400

    added server with basic routes

---
+++ routes/html-routes.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+require("./routes/html-routes.js")(app);
+
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
